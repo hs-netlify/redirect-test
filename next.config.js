afdfe1dff8517c44bce1122a1d9fd437ab0fd5ef
Henry Smith <henry.smith@netlify.com>
2022-06-08 13:48:30 +0100

    Switch to next.config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
 
-module.exports = nextConfig
+  async rewrites() {
+    return {
+      fallback: [
+        {
+          source: "/reviews/:slug*",
+          destination: `https://www.photographyblog.com/reviews/:slug*`,
+        },
+        {
+          source: "/:slug*",
+          destination: `https://www.photographyblog.com/:slug*`,
+        },
+      ],
+    };
+  },
+};
+
+module.exports = nextConfig;

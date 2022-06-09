a5e01d1e4b05099588f22d2391bea1c4c5cf6780
Henry Smith <henry.smith@netlify.com>
2022-06-09 11:31:27 +0100

    Test plugin

--- plugins/netlify-plugin-combined-sitemap/index.js
+++ plugins/netlify-plugin-combined-sitemap/index.js
@@ -1,5 +1,66 @@
+/* Generates a sitemap */
+const { env } = require("process");
+
+const makeSitemap = require("./make_sitemap");
+
+const fetch = require("node-fetch");
+
+const getInputsDir = ({ inputs }) =>
+  inputs.dir || inputs.distPath || inputs.buildDir;
+
+const getBuildDir = ({ inputs, constants }) => {
+  // Backwards compat... Correct opt is buildDir
+  const buildDir = getInputsDir({ inputs }) || constants.PUBLISH_DIR;
+  // constants.PUBLISH_DIR is always an absolute path
+  if (buildDir === constants.PUBLISH_DIR) {
+    return buildDir;
+  }
+  // remove leading / to treat the dir a a relative one
+  const trimmedBuildDir = buildDir.startsWith("/")
+    ? buildDir.slice(1)
+    : buildDir;
+  return trimmedBuildDir || ".";
+};
+
+const getLegacySitemap = async (baseUrl) => {
+  const legacySitemapRaw = await (await fetch(`${baseUrl}/sitemap.xml`)).text();
+
+  const parser = new DOMParser();
+  const legacySitemap = parser.parseFromString(
+    legacySitemapRaw,
+    "application/xml"
+  );
+  console.log("sitemap", legacySitemap);
+};
+
 module.exports = {
-  onPostBuild: async () => {
-    console.log("Hello world from onPreBuild event!");
+  onPostBuild: async ({ constants, inputs, utils }) => {
+    const legacyUrl =
+      env.NETLIFY_PLUGIN_SITEMAP_LEGACY_APP_URL || env.LEGACY_APP_URL;
+    const baseUrl =
+      inputs.baseUrl || env.NETLIFY_PLUGIN_SITEMAP_BASEURL || env.URL;
+    const urlPrefix =
+      inputs.urlPrefix || env.NETLIFY_PLUGIN_SITEMAP_URL_PREFIX || null;
+    const buildDir = getBuildDir({ inputs, constants });
+
+    if (legacyUrl) {
+      const legacySitemap = getLegacySitemap(baseUrl);
+    }
+    console.log("Creating sitemap from files...");
+
+    const data = await makeSitemap({
+      fileName: inputs.filePath,
+      homepage: baseUrl,
+      distPath: buildDir,
+      exclude: inputs.exclude,
+      prettyURLs: inputs.prettyURLs,
+      changeFreq: inputs.changeFreq,
+      priority: inputs.priority,
+      trailingSlash: inputs.trailingSlash,
+      failBuild: utils.build.failBuild,
+      urlPrefix,
+    });
+
+    console.log("Sitemap Built!", data.sitemapPath);
   },
 };

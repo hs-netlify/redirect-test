84b71ad69fbd9dc2a37fa6ba3179ea4a8670ac2d
Henry Smith <henry.smith@netlify.com>
2022-06-13 15:05:33 +0100

    Test deploy context

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -3,6 +3,6 @@ import { NextRequest, NextResponse } from "next/server";
 export async function middleware(req) {
   const res = NextResponse.next();
   res.cookie("CONTEXT", process.env.CONTEXT_NAME);
-
+  //TEst
   return res;
 }

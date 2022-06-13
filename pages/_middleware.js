81e4e94d7916083b16aa98b4ade051e2af8a658d
Henry Smith <henry.smith@netlify.com>
2022-06-13 13:53:19 +0100

    Middleware rebuild

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -2,5 +2,5 @@ export async function middleware(req) {
   // Add this line
   req.geo.country = req.headers.get("x-country");
 
-  // The rest of your middleware goes here
+  // The rest of your middleware goes here rebuild
 }
